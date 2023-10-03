6d3cdeb16606d7d36de0d625100e454a270a883a
JASWANTH <100197146+jash1718@users.noreply.github.com>
2023-10-03 21:30:26 +0530

    Add files via upload

---
+++ Leetcode/Plusone.java
@@ -0,0 +1,18 @@
+class Plusone{
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        {
+            for (int i = n - 1; i >= 0; i--) {
+                if (digits[i] < 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] number = new int[n + 1];
+            number[0] = 1;
+            return number;
+        }
+
+    }
+}
